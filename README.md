19093660dfeaec88caf054f0e7a3d1096d42cfff
Jacarri Chan <772929989@qq.com>
2014-12-17 13:15:11 +0800

    ADD comment  about  MIUI and flyme
    Signed-off-by: Jacarri Chan <772929989@qq.com>

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ This demo application demonstrates how to set an alarm using the Android AlarmMa
 In this example, we set an alarm that goes off five seconds after being launched.  The alarming activity then plays the default alarm on the device.
 
 To read a blog post about this demo, [check it out here](http://nerdwin15.com/2013/04/android-creating-an-alarm-with-alarmmanager/).
+
+```
+这个应用在小米 'MIUI-JXDCNBA9.0'和魅族 “flyme OS 2.5.2(A12328)”上工作不正常，原因是定制系统对原生Android有修改，使得AlarmManager工作不正常
+表现为AlarmManager指向的PendingIntent不能在指定时间唤醒
+
+
+```
--- src/com/nerdwin15/demo/alarmdemo/AlarmReceiverActivity.java
+++ src/com/nerdwin15/demo/alarmdemo/AlarmReceiverActivity.java
@@ -1,9 +1,13 @@
 package com.nerdwin15.demo.alarmdemo;
 
 import java.io.IOException;
+import java.util.Calendar;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
@@ -22,57 +26,70 @@ import android.widget.Button;
  * @author Michael Irwin
  */
 public class AlarmReceiverActivity extends Activity {
-    private MediaPlayer mMediaPlayer; 
+	private MediaPlayer mMediaPlayer;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.alarm);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.alarm);
 
-        Button stopAlarm = (Button) findViewById(R.id.stopAlarm);
-        stopAlarm.setOnTouchListener(new OnTouchListener() {
-            public boolean onTouch(View arg0, MotionEvent arg1) {
-                mMediaPlayer.stop();
-                finish();
-                return false;
-            }
-        });
+		Button stopAlarm = (Button) findViewById(R.id.stopAlarm);
+		// ===================================
+		// Create an offset from the current time in which the alarm will go
+		// off.
+		final Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.SECOND, 5);
+		// Create a new PendingIntent and add it to the AlarmManager
+		Intent intent = new Intent(this, AlarmReceiverActivity.class);
+		final PendingIntent pendingIntent = PendingIntent.getActivity(this,
+				12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+		stopAlarm.setOnTouchListener(new OnTouchListener() {
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				mMediaPlayer.stop();
+				finish();
+				// ===================================
+				AlarmManager am = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
+				am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+						pendingIntent);
+				// ===================================
+				return false;
+			}
+		});
 
-        playSound(this, getAlarmUri());
-    }
+		playSound(this, getAlarmUri());
+	}
 
-    private void playSound(Context context, Uri alert) {
-        mMediaPlayer = new MediaPlayer();
-        try {
-            mMediaPlayer.setDataSource(context, alert);
-            final AudioManager audioManager = (AudioManager) context
-                    .getSystemService(Context.AUDIO_SERVICE);
-            if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
-                mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
-                mMediaPlayer.prepare();
-                mMediaPlayer.start();
-            }
-        } catch (IOException e) {
-            System.out.println("OOPS");
-        }
-    }
+	private void playSound(Context context, Uri alert) {
+		mMediaPlayer = new MediaPlayer();
+		try {
+			mMediaPlayer.setDataSource(context, alert);
+			final AudioManager audioManager = (AudioManager) context
+					.getSystemService(Context.AUDIO_SERVICE);
+			if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+				mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+				mMediaPlayer.prepare();
+				mMediaPlayer.start();
+			}
+		} catch (IOException e) {
+			System.out.println("OOPS");
+		}
+	}
 
-    //Get an alarm sound. Try for an alarm. If none set, try notification, 
-    //Otherwise, ringtone.
-    private Uri getAlarmUri() {
-        Uri alert = RingtoneManager
-                .getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if (alert == null) {
-            alert = RingtoneManager
-                    .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            if (alert == null) {
-                alert = RingtoneManager
-                        .getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-            }
-        }
-        return alert;
-    }
+	// Get an alarm sound. Try for an alarm. If none set, try notification,
+	// Otherwise, ringtone.
+	private Uri getAlarmUri() {
+		Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+		if (alert == null) {
+			alert = RingtoneManager
+					.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			if (alert == null) {
+				alert = RingtoneManager
+						.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+			}
+		}
+		return alert;
+	}
 }
